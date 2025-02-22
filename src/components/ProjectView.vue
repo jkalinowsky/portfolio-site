f9ec2375b8181bf9818fe903ede5a86f1e326a39
kbkl <j.kalinowskyy@gmail.com>
2025-02-23 00:33:55 +0100

    Projects and about me

--- src/components/ProjectView.vue
+++ src/components/ProjectView.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
   import P1View from '@/components/projects/P1View.vue'
   import P2View from '@/components/projects/P2View.vue'
+  import P3View from '@/components/projects/P3View.vue'
   import { markRaw, ref } from 'vue'
 
   const projects = [
     { name: "Project1", component: markRaw(P1View) },
     { name: "Project2", component: markRaw(P2View) },
+    { name: "Project3", component: markRaw(P3View) }
   ];
 
   const currentIndex = ref(0);
   const currentProject = ref(projects[currentIndex.value].component);
-  const isNavOpen = ref(false);
 
   const changeProject = (direction: 'prev' | 'next') => {
     if (direction === 'prev') {
@@ -23,31 +24,20 @@
  </script>
 
 <template>
-  <button class="nav-toggle" @click="isNavOpen = !isNavOpen">☰</button>
   <div class="container">
-
-    <transition name="slide">
-      <nav v-if="isNavOpen" class="navbar">
-        <ul>
-          <li> Home </li>
-          <li> About me </li>
-          <li> Contact </li>
-        </ul>
-      </nav>
-    </transition>
-
     <div class="project-header">
       <button @click="changeProject('prev')" class="arrow-button left">&#9665;</button>
       <h1 class="h-projects">Projects</h1>
       <button @click="changeProject('next')" class="arrow-button right">&#9655;</button>
     </div>
-    <keep-alive>
-      <component :is="currentProject"  :changeProject="changeProject" />
-    </keep-alive>
+    <div class="content">
+      <keep-alive>
+        <component :is="currentProject"  :changeProject="changeProject" />
+      </keep-alive>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   @import "@/assets/styles/projects.scss";
-  @import "@/assets/styles/navbar.scss";
 </style>
